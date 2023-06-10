761c264f6cad0d54f673509af09aa162737179c5
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-06-10 19:22:15 +0000

    constf

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,8 +1,10 @@
 //application Name
+import 'package:awesome_select/awesome_select.dart';
+
 const appName = "Victoire";
 //Fields Name
-const pirstName = "Prénom";
-const pastName = "Nom";
+const firstName = "Prénom";
+const lastName = "Nom";
 const phone = "Numéro de téléphone";
 const mail = "E-mail";
 const username = "Nom d'utilisateur";
@@ -11,13 +13,40 @@ const address = "Adresse";
 const password = "Mot de passe";
 const creationDate = "Date de création du compte";
 const birthDay = "Date de naissances";
-const be_patient = "Veuillez patienter traitement en cours...";
-const signUpSuccessMessage = "Votre compte à été créer avec succès";
-const signUpErrorMessage = "Erreur survenu lors de la création de votre compte";
+const bePatient = "Veuillez patienter traitement en cours...";
+
+const town = "ville";
+
+//
+const welcome = "Accueil";
+const letStart = "Commençons";
+const signUpSuccessMessage = "votre Compte a été créé avec succès";
+const succesMessage = "Opération effectué avec succès";
+const String errorOccured =
+    "Une erreur est survenue veuillez réessayez plus tard";
 const signInSuccessMessage = "Connexion réussi";
 const signInErrorMessage = "Erreur survenu lors de la connexion à votre compte";
 const forgetPassword = "Mot de passe oublier";
-const haveAccountSignIn = "J'ai déja un compte";
+const haveAccountSignIn = "J'ai déjà un compte";
 const signIn = "S'identifier";
 const haveAccountSignUp = "Je n'ai pas compte";
-const signUp = "Crée un compte";
+const signUp = "Créer un compte";
+const confirmPassword = "Confirmez le mot de passe";
+//object Namea
+const panierUpper = "Panier";
+const panierlawer = "Panier";
+const produitUpper = "Produit";
+const produitlawer = "produit";
+const contactUpper = "Contact";
+const contactlawer = "contact";
+const catgorieUpper = "Catégorie";
+const categorielawer = "catégorie";
+const commandeUpper = "Commande";
+const commandelawer = "commande";
+const actuUpper = "Actualités";
+const actulawer = "actualités";
+
+List<S2Choice<String>> genderOptions = [
+  S2Choice<String>(value: 'HOMME', title: 'Homme'),
+  S2Choice<String>(value: 'FEMME', title: 'Femme'),
+];
