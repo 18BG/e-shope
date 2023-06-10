a2547c247e0270771f24c3523d7105db8873f791
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-06-10 12:42:24 +0000

    const

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,5 +1,14 @@
+//application Name
+const appName = "Victoire";
 //Fields Name
-const FirstName = "Prénom";
-const LastName = "Nom";
-const Phone = "Numéro de téléphone";
+const pirstName = "Prénom";
+const pastName = "Nom";
+const phone = "Numéro de téléphone";
 const mail = "E-mail";
+const username = "Nom d'utilisateur";
+const profil = "Profil";
+const address = "Adresse";
+const password = "Mot de passe";
+const creationDate = "Date de création du compte";
+const BirthDay = "Date de naissances";
+const be_patient = "Veuillez patienter traitement en cours...";
