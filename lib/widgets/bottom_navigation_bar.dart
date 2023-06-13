a6cf244591d3997a9ee9182e1399e6f7678f708a
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-06-13 12:41:53 +0000

    bottomBar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -57,37 +57,41 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.black,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.white,
+          selectedLabelStyle: const TextStyle(fontSize: 16),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color.fromARGB(255, 92, 112, 93),
           selectedFontSize: 16,
-          unselectedFontSize: 10,
+          unselectedFontSize: 14,
           onTap: _onItemTapped,
           currentIndex: _selectedIndex,
-          items: const [
+          items: [
             BottomNavigationBarItem(
                 icon: Icon(
                   Icons.home,
-                  color: Colors.black,
+                  color: _selectedIndex == 0 ? Colors.white : Colors.black,
                 ),
                 label: 'Accueil',
                 tooltip: 'Accueil'),
             BottomNavigationBarItem(
                 icon: Icon(
                   Icons.chat,
-                  color: Colors.black,
+                  color: _selectedIndex == 1 ? Colors.white : Colors.black,
                 ),
                 label: 'Discussion',
                 tooltip: 'Chat'),
             BottomNavigationBarItem(
                 icon: Icon(
                   Icons.shopping_cart_outlined,
-                  color: Colors.black,
+                  color: _selectedIndex == 2 ? Colors.white : Colors.black,
                 ),
                 label: 'Chariot',
                 tooltip: 'collecté'),
             BottomNavigationBarItem(
                 icon: Icon(
                   Icons.person_2_outlined,
-                  color: Colors.black,
+                  color: _selectedIndex == 3 ? Colors.white : Colors.black,
                 ),
                 label: 'Profil',
                 tooltip: 'profil'),
