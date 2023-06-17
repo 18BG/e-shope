8fcb2fafedc6405724160c82cb839e0e5d253a67
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-06-17 11:56:32 +0000

    products

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_shope/utilities/constants.dart';
 import 'package:e_shope/widgets/input.dart';
 import 'package:flutter/material.dart';
 
@@ -15,35 +16,83 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Form(
-        key: key,
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(12),
-              child: const Text(
-                "Créez un compte personnelle",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        //padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: Form(
+          key: key,
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(12),
+                child: const Text(
+                  signUp,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextFormFields(
-                hint: "Password",
-                hide: true,
-                suffix: true,
-                prefix: false,
+              NameTextField(),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormFields(
+                  hint: "Password",
+                  hide: true,
+                  suffix: true,
+                  prefix: false,
+                ),
               ),
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                child: isLoading
-                    ? const CircularProgressIndicator()
-                    : const Text("Créer un compte"))
-          ],
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormFields(
+                  hint: "Password",
+                  hide: true,
+                  suffix: true,
+                  prefix: false,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: isLoading
+                      ? const CircularProgressIndicator()
+                      : const Text("Créer un compte"))
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class NameTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.grey),
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormFields(
+              hint: "Password",
+              hide: true,
+              suffix: true,
+              prefix: false,
+            ),
+          ),
+          Expanded(
+            child: TextFormFields(
+              hint: "Password",
+              hide: true,
+              suffix: true,
+              prefix: false,
+            ),
+          ),
+        ],
       ),
     );
   }
